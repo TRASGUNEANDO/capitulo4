cfd8302c55499f36cf46e31b5aa9bf134c627d77
Raquel <raqdopsai@alu.edu.gva.es>
2024-02-11 16:54:38 +0100

    Primera subida ejercicio inicial

--- src/Criba.java
+++ src/Criba.java
@@ -1,60 +1,86 @@
-import java.util.Scanner;
+import java.util.Arrays;
 
-    public class Criba {
-         // Generar números primos de 1 a max
-        public static int[] generarPrimos (int max)
-        {
-            int i,j;
-            if (max >= 2) {
-// Declaraciones
-                int dim = max + 1; // Tamaño del array
-                boolean[] esPrimo = new boolean[dim];
-// Inicializar el array
-                for (i=0; i<dim; i++)
-                    esPrimo[i] = true;
-// Eliminar el 0 y el 1, que no son primos
-                esPrimo[0] = esPrimo[1] = false;
-// Criba
-                for (i=2; i<Math.sqrt(dim)+1; i++) {
-                    if (esPrimo[i]) {
-// Eliminar los múltiplos de i
-                        for (j=2*i; j<dim; j+=i)
-                            esPrimo[j] = false;
-                    }
-                }
-// ¿Cuántos primos hay?
-                int cuenta = 0;
-                for (i=0; i<dim; i++) {
-                    if (esPrimo[i])
-                        cuenta++;
-                }
-// Rellenar el vector de números primos
-                int[] primos = new int[cuenta];
-                for (i=0, j=0; i<dim; i++) {
-                    if (esPrimo[i])
-                        primos[j++] = i;
-                }
-                return primos;
-            } else { // max < 2
-                return new int[0];
-// Vector vacío
+public class Criba {
+
+    // Generar números primos de 1 a max
+    public static int[] generarPrimos(int max)
+    {
+        int[] primos; //array que contendrá los números primos obtenidos y que devolveremos
+
+        if (max >= 2) { //Si max < 2 no se pueden calcular los primos (no hay)
+
+            boolean[] esPrimo = new boolean[max+1]; //max+1 es el tamaño del array porque
+            //hay que añadir el 0
+            // Criba
+            calcularEsPrimo(esPrimo); //se verifica para cada números si es o no primo
+            primos = numerosPrimosHay(esPrimo); //se crea un array solo con los núm primos
+        } else { // max < 2
+            primos = null; //no hay primos no tiene que haber array
+            //así evitamos dos return
+        }
+        return primos;
+    }
+
+    //Devuelve un array con los números primos que hay en los primeros n números naturales
+    private static int[] numerosPrimosHay(boolean[] esPrimo) {
+        int j;
+        int i;
+        // ¿Cuántos primos hay?
+        final int cuenta = cuentaPrimos(esPrimo);
+
+        // Rellenar el vector de números primos
+        int[] primos = new int[cuenta];
+        for (i=0, j=0; i< esPrimo.length ; i++) {
+            if (esPrimo[i])
+                primos[j++] = i;
+        }
+        return primos;
+    }
+
+    //Calcula en el array de números si son primos o no los números de los índices
+    //del array
+    private static void calcularEsPrimo(boolean[] esPrimo) {
+        int i;
+        int j;
+
+        Arrays.fill(esPrimo,true); //se inicia el array con "true" en todos casos
+        esPrimo[0] = esPrimo[1] = false; // Eliminar el 0 y el 1, que no son primos
+
+        for (i=2; i<Math.sqrt(esPrimo.length)+1; i++) {
+            for (j=2*i; j< esPrimo.length; j+=i)  {
+                esPrimo[j] = false;
             }
         }
-        public static void main(String[] args) {
-            Scanner teclado=new Scanner(System.in);
-            System.out.println("Introduce el número para la criba de Erastótenes:");
-            int dato=teclado.nextInt();
-            int[] vector=new int[dato];
-            System.out.println("\nVector inicial hasta :"+dato);
+    }
+
+    // devuelve la cantidad de números primos que hay en el array
+    private static int cuentaPrimos(boolean[] esPrimo) {
+        int i;
+        int cuenta = 0;
+        for (i=0; i< esPrimo.length; i++) {
+            if (esPrimo[i])
+                cuenta++;
+        }
+        return cuenta;
+    }
+    public static void mostrarElementosArray(int[] vector) {
+        if (vector!=null) {
             for (int i = 0; i < vector.length; i++) {
-                if (i%10==0) System.out.println();
-                System.out.print(i+1+"\t");
-            }
-            vector=generarPrimos(dato);
-            System.out.println("\nVector de primos hasta:"+dato);
-            for (int i = 0; i < vector.length; i++) {
-                if (i%10==0) System.out.println();
-                System.out.print(vector[i]+"\t");
+                if (i % 10 == 0) System.out.println();
+                System.out.print(vector[i] + "\t");
             }
         }
+    }
+
+    public static int[] tablaConsecutivos(int dato) {
+        int[] numeros = new int [dato];
+
+        if (dato>0) {
+            for (int i = 0; i < dato; i++) {
+                numeros[i] = i + 1;
+            }
+        } else numeros = null;
+        return numeros;
+    }
 }
+
---
+++ src/principal.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class principal {
+
+    public static void main(String[] args) {
+
+        //Se pide al usuario un número para calcular los primos que hay hasta él
+        Scanner teclado=new Scanner(System.in);
+        System.out.print("Introduce el número para la criba de Erastótenes:");
+        int dato=teclado.nextInt();
+
+        //Creamos un vector para comprobar los numeros comprendidos entre 1 y dato
+        int[] vector;
+
+        System.out.println("\nVector inicial hasta :"+dato);
+        int[] numConsecutivos = Criba.tablaConsecutivos(dato); //se genera un array
+        //con los 'dato' primeros números para poder mostrarlos a continuación
+        //esto solamente facilita la lectura del programa, no es más ágil
+
+        Criba.mostrarElementosArray(numConsecutivos); //se muestra el array de los números
+        //consecutivos de 10 en 10 (por fila)
+
+        vector = Criba.generarPrimos(dato); //se genera un array conteniendo los primos
+        //existentes entre 1 y 'dato'
+
+        System.out.println("\nVector de primos hasta:"+dato);
+        Criba.mostrarElementosArray(vector); //se muestran los datos contenidos en el array
+    }
+}
